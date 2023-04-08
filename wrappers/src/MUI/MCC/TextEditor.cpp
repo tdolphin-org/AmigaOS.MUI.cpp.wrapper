6d11b2db25e296dadbc39a6a11ca820434709f81
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2023-04-08 23:16:59 +0200

    MUI::MCC::TextEditor wrapper class

---
+++ wrappers/src/MUI/MCC/TextEditor.cpp
@@ -0,0 +1,61 @@
+//
+//  AmigaOS MUI C++ wrapper
+//
+//  (c) TDolphin 2022-2023
+//
+
+#include "TextEditor.hpp"
+
+#include <mui/TextEditor_mcc.h>
+
+namespace MUI::MCC
+{
+    const std::string TextEditor::className = MUIC_TextEditor;
+
+    TextEditorBuilder::TextEditorBuilder()
+      : BuilderRoot(MUI::EmptyUniqueId, TextEditor::className)
+    {
+    }
+
+    TextEditor &TextEditor::setContents(const std::string &contents)
+    {
+        SetValue(MUIA_TextEditor_Contents, contents);
+        return *this;
+    }
+
+    TextEditor &TextEditor::Clear()
+    {
+        SetValue(MUIA_TextEditor_Contents, nullptr);
+        return *this;
+    }
+
+    std::string TextEditor::ExportText() const
+    {
+        char *text = (char *)DoMethod(muiObject(), MUIM_TextEditor_ExportText);
+        return std::string(text);
+    }
+
+    TextEditorBuilder &TextEditorBuilder::tagContents(const std::string &contents)
+    {
+        PushTag(MUIA_TextEditor_Contents, contents);
+        return *this;
+    }
+
+    TextEditorBuilder &TextEditorBuilder::tagRows(const long rows)
+    {
+        PushTag(MUIA_TextEditor_Rows, rows);
+        return *this;
+    }
+
+    TextEditorBuilder &TextEditorBuilder::tagSlider(const MUI::Root &slider)
+    {
+        PushTag(MUIA_TextEditor_Slider, slider.muiObject());
+        return *this;
+    }
+
+    TextEditorBuilder &TextEditorBuilder::tagSlider(const Object *pSlider)
+    {
+        PushTag(MUIA_TextEditor_Slider, pSlider);
+        return *this;
+    }
+}
---
+++ wrappers/src/MUI/MCC/TextEditor.hpp
@@ -0,0 +1,52 @@
+//
+//  AmigaOS MUI C++ wrapper
+//
+//  (c) TDolphin 2022-2023
+//
+
+#pragma once
+
+#include "MUI/Core/BuilderRoot.hpp"
+#include "MUI/Core/Root.hpp"
+
+namespace MUI::MCC
+{
+    class TextEditor : public MUI::Root // @todo check if custom classes are derived from Root or Notify?
+    {
+
+      public:
+        TextEditor(const Object *pObject)
+          : Root(pObject)
+        {
+        }
+
+        const static std::string className;
+
+        // get/set (attributes), all setters return object reference
+
+        /// @brief [ @b MUIA_TextEditor_Contents ]
+        TextEditor &setContents(const std::string &contents);
+
+        // methods, some can return object reference
+
+        TextEditor &Clear();
+
+        /// @brief [ @b MUIM_TextEditor_ExportText ]
+        std::string ExportText() const;
+    };
+
+    class TextEditorBuilder : public BuilderRoot<TextEditor>
+    {
+      public:
+        TextEditorBuilder();
+
+        /// @brief [ @b MUIA_TextEditor_Contents ]
+        TextEditorBuilder &tagContents(const std::string &contents);
+        /// @brief [ @b MUIA_TextEditor_Rows ]
+        TextEditorBuilder &tagRows(const long rows);
+        /// @brief [ @b MUIA_TextEditor_Slider ]
+        TextEditorBuilder &tagSlider(const MUI::Root &slider);
+        /// @brief [ @b MUIA_TextEditor_Slider ]
+        TextEditorBuilder &tagSlider(const Object *pSlider);
+    };
+}
