8e56c8f5fed6843aff5b59b31d219d25b3f6918c
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2023-05-13 23:41:43 +0200

    update MUI::DestGroupNotifier

--- wrappers/src/MUI/Notifier/DestGroupNotifier.cpp
+++ wrappers/src/MUI/Notifier/DestGroupNotifier.cpp
@@ -6,6 +6,8 @@
 
 #include "DestGroupNotifier.hpp"
 
+#include <proto/alib.h>
+
 #include "MUI/Group.hpp"
 
 namespace MUI
@@ -13,5 +15,17 @@ namespace MUI
     DestGroupNotifier::DestGroupNotifier(const SourceNotifier &sourceNotifier, const Group &group)
       : DestAreaNotifier(sourceNotifier, group)
     {
+    }
+
+    DestGroupNotifier &DestGroupNotifier::setActivePage(const long activePage, bool notify)
+    {
+        DoMethod(muiSourceObject(), MUIM_Notify, getAttribute(), getTrigValue(), mObject.muiObject(), 3,
+                 notify ? MUIM_Set : MUIM_NoNotifySet, MUIA_Group_ActivePage, activePage);
+    }
+
+    DestGroupNotifier &DestGroupNotifier::setActivePage(const GroupActivePage activePage, bool notify)
+    {
+        DoMethod(muiSourceObject(), MUIM_Notify, getAttribute(), getTrigValue(), mObject.muiObject(), 3,
+                 notify ? MUIM_Set : MUIM_NoNotifySet, MUIA_Group_ActivePage, (unsigned long)activePage);
     }
 }
--- wrappers/src/MUI/Notifier/DestGroupNotifier.hpp
+++ wrappers/src/MUI/Notifier/DestGroupNotifier.hpp
@@ -10,11 +10,19 @@
 
 namespace MUI
 {
+     enum class GroupActivePage;
+
     class DestGroupNotifier : public DestAreaNotifier
     {
         friend class SourceNotifier;
 
       protected:
         DestGroupNotifier(const SourceNotifier &sourceNotifier, const Group &group);
+
+      public:
+        /// @brief [ @b MUIM_Set / @b MUIM_NoNotifySet, @b MUIA_Group_ActivePage ]
+        DestGroupNotifier &setActivePage(const long activePage, bool notify = true);
+        /// @brief [ @b MUIM_Set / @b MUIM_NoNotifySet, @b MUIA_Group_ActivePage ]
+        DestGroupNotifier &setActivePage(const enum GroupActivePage activePage, bool notify = true);
     };
 }
