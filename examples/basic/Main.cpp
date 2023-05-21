c6e12de5a58a72f5955dc236e4ca7464d5e2b962
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2023-05-21 22:04:25 +0200

    update basic example

--- examples/basic/Main.cpp
+++ examples/basic/Main.cpp
@@ -20,30 +20,41 @@
 #include "MUI/Group.hpp"
 #include "MUI/Notifier/Core/Notifier.hpp"
 #include "MUI/Rectangle.hpp"
+#include "MUI/Text.hpp"
 #include "MUI/Window.hpp"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
-    cout << "basic MUI app example, created width 'AmigaOS MUI C++ wrapper'" << endl;
+    cout << "basic MUI app example, created with 'AmigaOS MUI C++ wrapper'" << endl;
 
     MuiMasterBaseScope muiBase;
 
-    auto window = MUI::WindowBuilder()
-                      .tagTitle("Window Title")
-                      .tagScreenTitle("Application Screen Name")
-                      .tagID("MAIN")
-                      .tagWidth(640)
-                      .tagHeight(200)
-                      .tagAltWidth(1024)
-                      .tagAltHeight(600)
-                      .tagRootObject(MUI::GroupBuilder()
-                                         .tagChild(MUI::RectangleBuilder().object())
-                                         .tagChild(MUI::MakeObject::SimpleButton("button"))
-                                         .tagChild(MUI::RectangleBuilder().object())
-                                         .object())
-                      .object();
+    auto closeButton = MUI::MakeObject::SimpleButton("_Quit Application");
+
+    auto window
+        = MUI::WindowBuilder()
+              .tagTitle("Window Title")
+              .tagScreenTitle("Application Screen Title")
+              .tagID("MAIN")
+              .tagWidth(640)
+              .tagHeight(200)
+              .tagAltWidth(1024)
+              .tagAltHeight(600)
+              .tagRootObject(MUI::GroupBuilder()
+                                 .tagChild(MUI::TextBuilder()
+                                               .tagContents(MUIX_C "some centered text in MUI::Text\n\n" MUIX_PH " (c) 2022-2023 TDolphin")
+                                               .object())
+                                 .tagChild(MUI::RectangleBuilder().object())
+                                 .tagChild(MUI::GroupBuilder()
+                                               .horizontal()
+                                               .tagChild(MUI::MakeObject::SimpleButton("_Left Button"))
+                                               .tagChild(closeButton)
+                                               .tagChild(MUI::MakeObject::SimpleButton("_Right Button"))
+                                               .object())
+                                 .object())
+              .object();
 
     auto app = MUI::ApplicationBuilder()
                    .tagAuthor("AUTHORS")
@@ -60,6 +71,7 @@ int main(int argc, char **argv)
 
     // events
     MUI::Notifier::from(window).onCloseRequest(true).destObject(app).returnIDQuit();
+    MUI::Notifier::from(MUI::Area(closeButton)).onPressed(false).destObject(app).returnIDQuit();
 
     // open window on constructor, close on destructor
     MUI::WindowScope windowScope(window);
