64a2355fc494d0576326b6bf356d9475c6ea7893
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2023-06-08 21:01:04 +0200

    update comments

--- wrappers/src/MUI/ValueTypes/Font.hpp
+++ wrappers/src/MUI/ValueTypes/Font.hpp
@@ -12,20 +12,20 @@ namespace MUI
 {
     enum class Font
     {
-        Inherit = MUIV_Font_Inherit,
-        Normal = MUIV_Font_Normal,
-        List = MUIV_Font_List,
-        Tiny = MUIV_Font_Tiny,
-        Fixed = MUIV_Font_Fixed,
-        Title = MUIV_Font_Title,
-        Big = MUIV_Font_Big,
-        Button = MUIV_Font_Button,
-        Slider = MUIV_Font_Slider,
-        Gauge = MUIV_Font_Gauge,
-        Menu = MUIV_Font_Menu,
-        Tab = MUIV_Font_Tab,
-        Bubble = MUIV_Font_Bubble,
-        Huge = MUIV_Font_Huge,
+        Inherit = MUIV_Font_Inherit, // the same font as the surrounding object, this is the default
+        Normal = MUIV_Font_Normal, // the normal font
+        List = MUIV_Font_List, // the font configured for lists
+        Tiny = MUIV_Font_Tiny, // the tiny font
+        Fixed = MUIV_Font_Fixed, // the fixed width font
+        Title = MUIV_Font_Title, // the font configured for group titles
+        Big = MUIV_Font_Big, // the big font
+        Button = MUIV_Font_Button, // the font configured for buttons
+        Slider = MUIV_Font_Slider, // the font configured for sliders
+        Gauge = MUIV_Font_Gauge, // the font configured for gauges
+        Menu = MUIV_Font_Menu, // the font configured for menus
+        Tab = MUIV_Font_Tab, // the font configured for tab buttons
+        Bubble = MUIV_Font_Bubble, // the font configured for bubble help
+        Huge = MUIV_Font_Huge, // the huge font
         Last = MUIV_Font_Last,
         Count = MUIV_Font_Count
     };
--- wrappers/src/MUI/ValueTypes/Frame.hpp
+++ wrappers/src/MUI/ValueTypes/Frame.hpp
@@ -13,15 +13,15 @@ namespace MUI
     enum class Frame
     {
         None = MUIV_Frame_None,
-        Button = MUIV_Frame_Button,
-        ImageButton = MUIV_Frame_ImageButton,
-        Text = MUIV_Frame_Text,
-        String = MUIV_Frame_String,
-        ReadList = MUIV_Frame_ReadList,
-        InputList = MUIV_Frame_InputList,
-        Prop = MUIV_Frame_Prop,
+        Button = MUIV_Frame_Button, // for standard buttons with text in it
+        ImageButton = MUIV_Frame_ImageButton, // for small buttons with images, e.g. the arrows of a scrollbar
+        Text = MUIV_Frame_Text, // for a text field, e.g. a status line display
+        String = MUIV_Frame_String, // for a string gadget
+        ReadList = MUIV_Frame_ReadList, // for a read only list
+        InputList = MUIV_Frame_InputList, // for a list that handles input (has a cursor)
+        Prop = MUIV_Frame_Prop, // for proportional gadgets
         Gauge = MUIV_Frame_Gauge,
-        Group = MUIV_Frame_Group,
+        Group = MUIV_Frame_Group, // for groups
         PopUp = MUIV_Frame_PopUp,
         Virtual = MUIV_Frame_Virtual,
         Slider = MUIV_Frame_Slider,
