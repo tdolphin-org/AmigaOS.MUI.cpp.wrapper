7974974d00060334c5fcb79af9f60aa4096b7dfb
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2023-06-10 23:35:16 +0200

    update MUI::MenuitemNotifier

--- wrappers/src/MUI/Notifier/Source/MenuitemNotifier.hpp
+++ wrappers/src/MUI/Notifier/Source/MenuitemNotifier.hpp
@@ -22,9 +22,23 @@ namespace MUI
 
         // notification methods
 
-        /// @brief [ @b MUIM_Notify, @b MUIA_Menuitem_Trigger == @b MUIV_EveryTime ]
-        SourceNotifier<Menuitem, DestMenuitemNotifier> onTriggerEveryTime();
+        /// @brief [ @b MUIM_Notify, @b MUIA_Menuitem_Checked ]
+        SourceNotifier<Menuitem, DestMenuitemNotifier> onChecked(bool checked);
+        /// @brief [ @b MUIM_Notify, @b MUIA_Menuitem_Checked == @b MUIV_EveryTime ]
+        SourceNotifier<Menuitem, DestMenuitemNotifier> onCheckedEveryTime();
+        /// @brief [ @b MUIM_Notify, @b MUIA_Menuitem_Checkit ]
+        SourceNotifier<Menuitem, DestMenuitemNotifier> onCheckit(bool checkit);
         /// @brief [ @b MUIM_Notify, @b MUIA_Menuitem_Checkit == @b MUIV_EveryTime ]
         SourceNotifier<Menuitem, DestMenuitemNotifier> onCheckitEveryTime();
+        /// @brief [ @b MUIM_Notify, @b MUIA_Menuitem_Enabled ]
+        SourceNotifier<Menuitem, DestMenuitemNotifier> onEnabled(bool enabled);
+        /// @brief [ @b MUIM_Notify, @b MUIA_Menuitem_Enabled == @b MUIV_EveryTime ]
+        SourceNotifier<Menuitem, DestMenuitemNotifier> onEnabledEveryTime();
+        /// @brief [ @b MUIM_Notify, @b MUIA_Menuitem_Toggle ]
+        SourceNotifier<Menuitem, DestMenuitemNotifier> onToggle(bool toggle);
+        /// @brief [ @b MUIM_Notify, @b MUIA_Menuitem_Toggle == @b MUIV_EveryTime ]
+        SourceNotifier<Menuitem, DestMenuitemNotifier> onToggleEveryTime();
+        /// @brief [ @b MUIM_Notify, @b MUIA_Menuitem_Trigger == @b MUIV_EveryTime ]
+        SourceNotifier<Menuitem, DestMenuitemNotifier> onTriggerEveryTime();
     };
 }
