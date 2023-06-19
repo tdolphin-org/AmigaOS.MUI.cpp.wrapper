281402c28948b184113d328bc7953ca37b2182d3
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2023-06-19 20:41:35 +0200

    update advanced example

---
+++ examples/advanced/Main.cpp
@@ -0,0 +1,23 @@
+//
+//  AmigaOS MUI C++ wrapper
+//
+//  Advanced Example
+//
+//  (c) 2022-2023 TDolphin
+//
+
+#include "Main.hpp
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    cout << "advanced MUI app example, created with 'AmigaOS MUI C++ wrapper'" << endl;
+
+    MuiMasterBaseScope muiBase;
+
+
+    return 0;
+}
