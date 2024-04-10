94f3ec76ac263681c602e6dd64ffd396b22291b4
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2024-04-10 20:41:28 +0200

    updated Advanced Example, fixed styles for MorphOS for MUI::Title

--- examples/advanced/Components/Tabs/TabsContainerTitles.cpp
+++ examples/advanced/Components/Tabs/TabsContainerTitles.cpp
@@ -14,7 +14,15 @@ namespace Components
 {
     TabsContainerTitles::TabsContainerTitles(const std::vector<std::pair<std::string, MUI::Area &>> &tabs)
       : mTabsTitle(MUI::TitleBuilder().tagClosable(true).tagNewable(true).object())
-      , mComponent(MUI::GroupBuilder().tagCycleChain().tagPageMode().tagChild(mTabsTitle).object())
+      , mComponent(MUI::GroupBuilder()
+            .tagCycleChain()
+            .tagChild(mTabsTitle)
+#ifdef __MORPHOS__
+            .tagPageMode()
+            .tagBackground(MUI::ImageOrBackground::RegisterBack)
+            .tagFrame(MUI::Frame::Register)
+#endif
+            .object())
     {
         for (auto &tab : tabs)
             mTabsTitle.AddTail(MUI::TextBuilder().tagContents(tab.first).object());
