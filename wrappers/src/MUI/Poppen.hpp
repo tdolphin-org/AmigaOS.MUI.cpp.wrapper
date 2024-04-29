0b0ea8c77b3b97b11097dd1df22d6d0ed18866ab
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2024-04-29 22:20:45 +0200

    updated MUI::Poppen

--- wrappers/src/MUI/Poppen.hpp
+++ wrappers/src/MUI/Poppen.hpp
@@ -35,6 +35,11 @@ namespace MUI
           : PendisplayBuilderTemplate<T, U>(uniqueId, muiClassName)
         {
         }
+
+        /// @brief [ @b MUIA_Window_Title ]
+        /// You can control the window title of the popup window using the MUIA_Window_Title on the Poppen object. It will remember its
+        /// value and use it when creating the popup window.
+        T &tagWindowTitle(const std::string &title);
     };
 
     class PoppenBuilder : public PoppenBuilderTemplate<PoppenBuilder, Poppen>
@@ -42,4 +47,11 @@ namespace MUI
       public:
         PoppenBuilder();
     };
+
+    template <typename T, typename U> inline T &PoppenBuilderTemplate<T, U>::tagWindowTitle(const std::string &title)
+    {
+        auto copy = this->StoreString(MUIA_ShortHelp, title);
+        this->PushTag(MUIA_Window_Title, copy);
+        return (T &)*this;
+    }
 }
