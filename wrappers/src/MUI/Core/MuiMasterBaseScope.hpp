1f530f46379ce2c1ee73a7a6e9286d7f3e3e9388
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2024-09-22 11:00:20 +0200

    refactor MuiMasterBaseScope

--- wrappers/src/MUI/Core/MuiMasterBaseScope.hpp
+++ wrappers/src/MUI/Core/MuiMasterBaseScope.hpp
@@ -4,9 +4,21 @@
 //  (c) 2022-2024 TDolphin
 //
 
+#include <exec/libraries.h>
+
 class MuiMasterBaseScope
 {
   public:
     MuiMasterBaseScope();
     ~MuiMasterBaseScope();
+
+    // copy is forbidden
+    MuiMasterBaseScope(const MuiMasterBaseScope &) = delete;
+    MuiMasterBaseScope &operator=(const MuiMasterBaseScope &) = delete;
+
+    // move is default
+    MuiMasterBaseScope(MuiMasterBaseScope &&other) noexcept = default;
+    MuiMasterBaseScope &operator=(MuiMasterBaseScope &&other) noexcept = default;
+
+    Library *library() const;
 };
