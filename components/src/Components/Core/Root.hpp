e74926ecc0ad013d8c793ffc276fbf2368967323
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2024-11-03 14:47:41 +0100

    refactor Components::Root, add Components::NotifyRoot

--- components/src/Components/Core/Root.hpp
+++ components/src/Components/Core/Root.hpp
@@ -24,7 +24,8 @@ namespace Components
         long MuiRequest(const std::string &title, const std::string &message, const std::string &gadgets) const;
     };
 
-    template <typename T> class Root : public AbstractRoot
+    /// @brief Root class for all components (including Application, Windows).
+    template <typename T> class NotifyRoot : public AbstractRoot
     {
       public:
         Object *muiObject() const
@@ -38,5 +39,20 @@ namespace Components
         }
 
         virtual T &muiRoot() const = 0;
+    };
+
+    /// @brief Root class for all components builded on at least MUI::Area (base class with MUIA_Disabled attribute)
+    template <typename T> class Root : public NotifyRoot<T>
+    {
+      public:
+        void Enable()
+        {
+            this->muiRoot().Enable();
+        }
+
+        void Disable()
+        {
+            this->muiRoot().Disable();
+        }
     };
 }
