0590e54475c91590c63c5abc23c9d7942b895b5b
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-09-05 21:43:35 +0200

    update StringStorage: extend functionality to manage string arrays and refactor memory management

--- wrappers/src/Core/StringStorage.cpp
+++ wrappers/src/Core/StringStorage.cpp
@@ -1,7 +1,7 @@
 //
 //  AmigaOS MUI C++ wrapper
 //
-//  (c) 2022-2024 TDolphin
+//  (c) 2022-2025 TDolphin
 //
 
 #include "StringStorage.hpp"
@@ -21,6 +21,11 @@ StringStorageCore::StringStorageCore()
 {
 }
 
+StringStorageCore::~StringStorageCore()
+{
+    ClearAll();
+}
+
 const char *StringStorageCore::Add(unsigned long &objectId, Tag tagName, const std::string &string)
 {
 #ifdef TRACE_SSC
@@ -30,9 +35,7 @@ const char *StringStorageCore::Add(unsigned long &objectId, Tag tagName, const s
     if (objectId == 0)
         objectId = ++mObjectIdCounter;
 
-    auto pString = std::shared_ptr<char>(new char[string.size() + 1], std::default_delete<char[]>());
-    std::memcpy(pString.get(), string.c_str(), string.size() + 1);
-
+    // Check if tagName already exists for this objectId
     const auto &outerIterator = mObjectIdToMap.find(objectId);
     if (outerIterator != mObjectIdToMap.end())
         if (outerIterator->second.find(tagName) != outerIterator->second.end())
@@ -42,9 +45,51 @@ const char *StringStorageCore::Add(unsigned long &objectId, Tag tagName, const s
             throw std::runtime_error(error);
         }
 
-    mObjectIdToMap[objectId][tagName] = pString;
+    // Create copy of string
+    auto pString = new char[string.size() + 1];
+    std::memcpy(pString, string.c_str(), string.size() + 1);
 
-    return (char *)pString.get();
+    mObjectIdToMap[objectId][tagName] = (const char *)pString; // map objectId and tagName to string copy
+
+    return (const char *)pString;
+}
+
+const char **StringStorageCore::Add(unsigned long &objectId, Tag tagName, const std::vector<std::string> &strings)
+{
+#ifdef TRACE_SSC
+    std::cout << "SSC::AddArray(objectId = " << objectId << ", tag = " << AOS::TagUtil::toString(tagName)
+              << ", strings.size() = " << strings.size() << ")" << std::endl;
+#endif
+
+    if (objectId == 0)
+        objectId = ++mObjectIdCounter;
+
+    // Check if tagName already exists for this objectId
+    const auto &outerIterator = mObjectIdToArrayMap.find(objectId);
+    if (outerIterator != mObjectIdToArrayMap.end())
+        if (outerIterator->second.find(tagName) != outerIterator->second.end())
+        {
+            auto error = "String Storage Collector: AddArray(" + std::to_string(objectId) + "," + AOS::TagUtil::toString(tagName)
+                + ") .. tagName for objectId already exists!";
+            throw std::runtime_error(error);
+        }
+
+    // Create array of char* pointers (with null terminator)
+    auto pArray = new char *[strings.size() + 1];
+
+    // Create copies of individual strings and store pointers
+    for (size_t i = 0; i < strings.size(); i++)
+    {
+        auto pString = new char[strings[i].size() + 1];
+        std::memcpy(pString, strings[i].c_str(), strings[i].size() + 1);
+
+        pArray[i] = pString;
+    }
+    pArray[strings.size()] = nullptr; // Null-terminate the array
+
+    mObjectIdToArrayMap[objectId][tagName] = (const char **)pArray; // map objectId and tagName to array of string copies
+
+    return (const char **)(pArray);
 }
 
 void StringStorageCore::Invalidate(const unsigned long objectId)
@@ -59,14 +104,23 @@ void StringStorageCore::Invalidate(const unsigned long objectId)
         throw std::runtime_error(error);
     }
 
-    const auto &outerIterator = mObjectIdToMap.find(objectId);
-    if (outerIterator == mObjectIdToMap.end())
-    {
-        auto error = "String Storage Collector: Invalidate(" + std::to_string(objectId) + ") .. map not found for objectId!";
-        throw std::runtime_error(error);
-    }
+    // Copy pointers to garbage
+    const auto &stringIterator = mObjectIdToMap.find(objectId);
+    if (stringIterator != mObjectIdToMap.end())
+        for (const auto &elem : stringIterator->second)
+            mGarbageStrings.push_back(elem.second);
 
-    mObjectIdToMap.erase(outerIterator);
+    // Copy array pointers to garbage
+    const auto &arrayIterator = mObjectIdToArrayMap.find(objectId);
+    if (arrayIterator != mObjectIdToArrayMap.end())
+        for (const auto &elem : arrayIterator->second)
+            mGarbageStringArrays.push_back(elem.second);
+
+    // Remove from both maps
+    mObjectIdToMap.erase(stringIterator);
+    mObjectIdToArrayMap.erase(arrayIterator);
+
+    ClearGarbage(); // remove garbage strings and string arrays
 }
 
 void StringStorageCore::FinalizeObject(const unsigned long objectId, const Object *object)
@@ -82,31 +136,37 @@ void StringStorageCore::FinalizeObject(const unsigned long objectId, const Objec
         throw std::runtime_error(error);
     }
 
-    const auto &outerIterator = mObjectIdToMap.find(objectId);
-    if (outerIterator == mObjectIdToMap.end())
+    // Copy strings to final object
+    const auto &stringIterator = mObjectIdToMap.find(objectId);
+    if (stringIterator != mObjectIdToMap.end())
     {
-        auto error = "String Storage Collector: FinalizeObject(" + std::to_string(objectId) + ") .. map not found for objectId!";
-        throw std::runtime_error(error);
+        for (const auto &elem : stringIterator->second)
+            mObjectPtrToMap[reinterpret_cast<uintptr_t>(object)][elem.first] = elem.second;
+        mObjectIdToMap.erase(stringIterator);
     }
 
-    // copy to final object
-    for (const auto &elem : outerIterator->second)
-        mObjectPtrToMap[reinterpret_cast<uintptr_t>(object)][elem.first] = elem.second;
-
-    mObjectIdToMap.erase(outerIterator);
+    // Copy arrays to final object
+    const auto &arrayIterator = mObjectIdToArrayMap.find(objectId);
+    if (arrayIterator != mObjectIdToArrayMap.end())
+    {
+        for (const auto &elem : arrayIterator->second)
+            mObjectPtrToArrayMap[reinterpret_cast<uintptr_t>(object)][elem.first] = elem.second;
+        mObjectIdToArrayMap.erase(arrayIterator);
+    }
 }
 
-char *StringStorageCore::Change(const Object *object, Tag tagName, const std::string &string)
+const char *StringStorageCore::Change(const Object *object, Tag tagName, const std::string &string)
 {
 #ifdef TRACE_SSC
     std::cout << "SSC::Change(object = " << ToString::FromDataPointer(object) << ", tag = " << AOS::TagUtil::toString(tagName)
               << ", string = \"" << string << "\")" << std::endl;
 #endif
 
-    mGarbageStrings.clear(); // remove garbage strings
+    ClearGarbage(); // remove garbage strings and string arrays
 
-    auto pString = std::shared_ptr<char>(new char[string.size() + 1], std::default_delete<char[]>());
-    std::memcpy(pString.get(), string.c_str(), string.size() + 1);
+    // Create copy of string
+    auto pString = new char[string.size() + 1];
+    std::memcpy(pString, string.c_str(), string.size() + 1);
 
     const auto &outerIterator = mObjectPtrToMap.find(reinterpret_cast<uintptr_t>(object));
     if (outerIterator != mObjectPtrToMap.end())
@@ -121,5 +181,87 @@ char *StringStorageCore::Change(const Object *object, Tag tagName, const std::st
 
     mObjectPtrToMap[reinterpret_cast<uintptr_t>(object)][tagName] = pString;
 
-    return (char *)pString.get();
+    return (const char *)pString;
+}
+
+const char **StringStorageCore::Change(const Object *object, Tag tagName, const std::vector<std::string> &strings)
+{
+#ifdef TRACE_SSC
+    std::cout << "SSC::Change(object = " << ToString::FromDataPointer(object) << ", tag = " << AOS::TagUtil::toString(tagName)
+              << ", strings.size() = " << strings.size() << ")" << std::endl;
+#endif
+
+    ClearGarbage(); // remove garbage strings and string arrays
+
+    // Create array of char* pointers (with null terminator)
+    auto pArray = new char *[strings.size() + 1];
+
+    // Create copies of individual strings and store pointers
+    for (size_t i = 0; i < strings.size(); i++)
+    {
+        auto pString = new char[strings[i].size() + 1];
+        std::memcpy(pString, strings[i].c_str(), strings[i].size() + 1);
+
+        pArray[i] = pString;
+    }
+    pArray[strings.size()] = nullptr; // Null-terminate the array
+
+    const auto &outerIterator = mObjectPtrToArrayMap.find(reinterpret_cast<uintptr_t>(object));
+    if (outerIterator != mObjectPtrToArrayMap.end())
+    {
+        const auto &innerIterator = outerIterator->second.find(tagName);
+        if (innerIterator != outerIterator->second.end())
+        {
+            mGarbageStringArrays.push_back(innerIterator->second); // can't be removed now, becouse still is used, move pointer to garbage
+            outerIterator->second.erase(innerIterator);
+        }
+    }
+
+    mObjectPtrToArrayMap[reinterpret_cast<uintptr_t>(object)][tagName] = (const char **)pArray;
+
+    return (const char **)pArray;
+}
+
+void StringStorageCore::ClearGarbage()
+{
+    // Delete all garbage strings
+    for (const auto &str : mGarbageStrings)
+        delete[] str;
+    mGarbageStrings.clear();
+
+    // Delete all garbage string arrays
+    for (const auto &array : mGarbageStringArrays)
+    {
+        for (auto it = array; *it != nullptr; ++it)
+            delete[] *it;
+        delete[] array;
+    }
+    mGarbageStringArrays.clear();
+}
+
+void StringStorageCore::ClearAll()
+{
+    // Clear all managed strings and string arrays
+
+    for (auto &outerPair : mObjectIdToMap)
+        for (auto &innerPair : outerPair.second)
+            mGarbageStrings.push_back(innerPair.second);
+    mObjectIdToMap.clear();
+
+    for (auto &outerPair : mObjectPtrToMap)
+        for (auto &innerPair : outerPair.second)
+            mGarbageStrings.push_back(innerPair.second);
+    mObjectPtrToMap.clear();
+
+    for (auto &outerPair : mObjectIdToArrayMap)
+        for (auto &innerPair : outerPair.second)
+            mGarbageStringArrays.push_back(innerPair.second);
+    mObjectIdToArrayMap.clear();
+
+    for (auto &outerPair : mObjectPtrToArrayMap)
+        for (auto &innerPair : outerPair.second)
+            mGarbageStringArrays.push_back(innerPair.second);
+    mObjectPtrToArrayMap.clear();
+
+    ClearGarbage(); // remove garbage strings and string arrays
 }
--- wrappers/src/Core/StringStorage.hpp
+++ wrappers/src/Core/StringStorage.hpp
@@ -1,7 +1,7 @@
 //
 //  AmigaOS MUI C++ wrapper
 //
-//  (c) 2022-2024 TDolphin
+//  (c) 2022-2025 TDolphin
 //
 
 #pragma once
@@ -15,18 +15,25 @@
 #include <utility/tagitem.h>
 #include <vector>
 
-/// @brief String Storage Collector (SSC), collect copies of strings as char*
+/// @brief String Storage Collector (SSC), collect copies of strings as char*, also copies arrays of strings as char**
 class StringStorageCore
 {
     friend class td::Singleton<StringStorageCore>;
 
     unsigned long mObjectIdCounter;
-    std::vector<std::shared_ptr<char>> mGarbageStrings;
-    std::map<unsigned long, std::map<Tag, std::shared_ptr<char>>> mObjectIdToMap; // objectId -> map (tag -> value)
-    std::map<unsigned long, std::map<Tag, std::shared_ptr<char>>> mObjectPtrToMap; // object -> map (tag -> value)
+
+    std::vector<const char *> mGarbageStrings;
+    std::vector<const char **> mGarbageStringArrays;
+
+    std::map<unsigned long, std::map<Tag, const char *>> mObjectIdToMap; // objectId -> map (tag -> value)
+    std::map<unsigned long, std::map<Tag, const char *>> mObjectPtrToMap; // object -> map (tag -> value)
+
+    std::map<unsigned long, std::map<Tag, const char **>> mObjectIdToArrayMap; // objectId -> map (tag -> value)
+    std::map<unsigned long, std::map<Tag, const char **>> mObjectPtrToArrayMap; // object -> map (tag -> value)
 
   public:
     StringStorageCore();
+    ~StringStorageCore();
 
     /// @brief add string to SSC for not yet created object for given object id (> 0), or for 0 to generate new id
     /// if string for objectId and tagName already exits, method throws exception
@@ -35,19 +42,42 @@ class StringStorageCore
     /// @param string string to create copy
     /// @return pointer to string copy
     const char *Add(unsigned long &objectId, Tag tagName, const std::string &string);
+
+    /// @brief add array of strings to SSC for not yet created object for given object id (> 0), or for 0 to generate new id
+    /// if strings for objectId and tagName already exits, method throws exception
+    /// @param objectId object id or 0
+    /// @param tagName tag name/id
+    /// @param strings array of strings to create copies
+    /// @return pointer to array of string copies (string array where last element pointer is nullptr)
+    const char **Add(unsigned long &objectId, Tag tagName, const std::vector<std::string> &strings);
+
     /// @brief invalidate (erase) all strings for not yet created object
     /// @param objectId id of object
     void Invalidate(const unsigned long objectId);
+
     /// @brief finalize not yet created object id with created object ptr, in case of any error throws exception
     /// @param objectId object id != 0
     /// @param object pointer to created object != nullptr
     void FinalizeObject(const unsigned long objectId, const Object *object);
+
     /// @brief change string for given object and for given tag, old string is removed (if exists), created new copy for new string
     /// @param object object ptr
     /// @param tagName tag name/id
     /// @param string new string
     /// @return pointer to string copy
-    char *Change(const Object *object, Tag tagName, const std::string &string);
+    const char *Change(const Object *object, Tag tagName, const std::string &string);
+
+    /// @brief change array of strings for given object and for given tag, old strings are removed (if exists), created new copies for new
+    /// strings
+    /// @param object object ptr
+    /// @param tagName tag name/id
+    /// @param strings new strings
+    /// @return pointer to array of string copies (string array where last element pointer is nullptr)
+    const char **Change(const Object *object, Tag tagName, const std::vector<std::string> &strings);
+
+  private:
+    void ClearGarbage(); // clear garbage strings and string arrays
+    void ClearAll();     // clear all strings and string arrays
 };
 
 using StringStorage = td::Singleton<StringStorageCore>;
