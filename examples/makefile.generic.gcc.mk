510f63b5be7936648b7fe5b03f86613d309af0b8
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2025-10-04 20:20:48 +0200

    update makefile for examples

--- examples/makefile.generic.gcc.mk
+++ examples/makefile.generic.gcc.mk
@@ -8,16 +8,22 @@
 #  generic makefile for gcc
 #
 
-CPP_FLAGS = $(DEBUG_FLAGS) $(MORE_CPP_FLAGS) -I${AOSCPP_PATH}/wrappers/src -I$(PROJECT) -I../wrappers/src -O3
+CPP_FLAGS = $(DEBUG_FLAGS) $(MORE_CPP_FLAGS) -I${AOSCPP_PATH}/wrappers/src -I$(PROJECT) -I../wrappers/src -I../components/src -O3
 LFLAGS = -L../wrappers/lib/$(SUB_BUILD_PATH) $(MORE_LFLAGS) -lstdc++ -noixemul -Os
 
 dir_guard = mkdir -p $(@D)
 
 BINPATH = out/$(SUB_BUILD_PATH)
 
+MUI_COMPONENTS_PATH = ../components
+MUI_COMPONENTS_MODULES = Components/MCC Components/MCC/Core
+MUI_COMPONENTS_SRC_DIRS = $(addprefix $(MUI_COMPONENTS_PATH)/src/,$(MUI_COMPONENTS_MODULES))
+MUI_COMPONENTS_SRCS = $(foreach sdir,$(MUI_COMPONENTS_SRC_DIRS),$(wildcard $(sdir)/*.cpp))
+
 SRC_DIRS = $(shell echo $(MODULES) | tr ',' ' ')
 SRCS = $(wildcard $(PROJECT)/*.cpp) $(foreach sdir,$(SRC_DIRS),$(wildcard $(PROJECT)/$(sdir)/*.cpp))
-OBJS = $(patsubst %.cpp,obj/$(SUB_BUILD_PATH)/%.o,$(SRCS))
+OBJS = $(patsubst %.cpp,obj/$(SUB_BUILD_PATH)/%.o,$(SRCS))\
+	$(patsubst $(MUI_COMPONENTS_PATH)/src/%.cpp,$(MUI_COMPONENTS_PATH)/obj/$(SUB_BUILD_PATH)/%.o,$(MUI_COMPONENTS_SRCS))
 
 all: $(BINPATH)/$(APP_EXAMPLE)
 
@@ -34,8 +40,6 @@ obj/$(SUB_BUILD_PATH)/%.o: %.cpp %.hpp
 	$(dir_guard)
 	$(CPPC) $(CPP_FLAGS) -c $< -o $@
 
-clean :
-	rm -f obj/$(SUB_BUILD_PATH)/*.o
-	rm -f -R out/$(SUB_BUILD_PATH)/*
-
-rebuild : clean all
+$(MUI_COMPONENTS_PATH)/obj/$(SUB_BUILD_PATH)/%.o: $(MUI_COMPONENTS_PATH)/src/%.cpp $(MUI_COMPONENTS_PATH)/src/%.hpp
+	$(dir_guard)
+	$(CPPC) $(CPP_FLAGS) -c $< -o $@
