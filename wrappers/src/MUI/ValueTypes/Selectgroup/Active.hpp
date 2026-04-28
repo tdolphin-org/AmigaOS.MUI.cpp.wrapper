6e2a61f5bccc49ca738548e5d56f269045be5b73
Rafal Zabdyr <rafal.zabdyr@tdolphin.org>
2026-04-28 20:10:16 +0200

    refactor MUI::ValueTypes::Selectgroup_Active enum class

--- wrappers/src/MUI/ValueTypes/Selectgroup/Active.hpp
+++ wrappers/src/MUI/ValueTypes/Selectgroup/Active.hpp
@@ -11,14 +11,11 @@ namespace MUI
 {
     namespace ValueTypes
     {
-        namespace Selectgroup
+        enum class Selectgroup_Active : long
         {
-            enum class Active : long
-            {
-                Next = MUIV_Selectgroup_Active_Next,
-                Prev = MUIV_Selectgroup_Active_Prev
-            };
-        }
+            Next = MUIV_Selectgroup_Active_Next,
+            Prev = MUIV_Selectgroup_Active_Prev
+        };
     }
 }
 #endif // AOS_MUI_VERSION_5
